878ca1f6806dbc6291a3f26c5a225445a97f1f7a
Andrea <Andrea@Zxs.mshome.net>
2021-11-09 14:17:47 -0700

    preliminar

---
+++ src/main/java/cliente/Cliente.java
@@ -0,0 +1,17 @@
+package cliente;
+
+/**
+ *
+ * @author Eli
+ */
+public class Cliente {
+
+    public static void main(String[] args) {
+        
+        JframeCliente cliente = new JframeCliente();
+        cliente.setVisible(true);
+        
+
+    }
+
+}
